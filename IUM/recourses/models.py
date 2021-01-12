96d688b1e56f8ee6b790050db5cf97ce7d52aa42
Wojciech Moczydlowski <wojciech.moczydlowski@leancode.pl>
2021-01-12 01:57:42 +0100

    Save logs

--- IUM/recourses/models.py
+++ IUM/recourses/models.py
@@ -1,11 +1,22 @@
+import json
+from datetime import datetime
+from uuid import uuid4
 from flask_restful import Resource
 from services.recommendations.get_user_model import get_user_model
+from services.recommendations.write_logs import write_logs
 from services.models.models_controller import get_recommendations
 
 
 class Models(Resource):
     def get(self, user_id):
         model = get_user_model(user_id)
-        recommendations = get_recommendations(user_id, model)
 
-        return recommendations
+        id = str(uuid4())
+        date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        products = get_recommendations(user_id, model)
+
+        recommendation = {"id": id, "date": date, "user_id": user_id, "products": products}
+
+        write_logs(recommendation)
+
+        return recommendation
